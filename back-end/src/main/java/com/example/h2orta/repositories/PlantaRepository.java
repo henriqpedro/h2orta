29ff5f7e6e706d24fbada811ff40a6418465bf82
Pedro Henrique <pedropereira712@gmail.com>
2024-05-26 22:35:49 -0300

    Iniciando projeto React

--- back-end/src/main/java/com/example/h2orta/repositories/PlantaRepository.java
+++ back-end/src/main/java/com/example/h2orta/repositories/PlantaRepository.java
@@ -1,9 +1,10 @@
 package com.example.h2orta.repositories;
 
 import com.example.h2orta.models.Planta;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlantaRepository {
+public interface PlantaRepository extends JpaRepository<Planta, Long> {
 
 }
---
+++ back-end/src/main/java/com/example/h2orta/services/PlantaService.java
@@ -0,0 +1,14 @@
+package com.example.h2orta.services;
+
+import com.example.h2orta.repositories.PlantaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PlantaService {
+
+    public PlantaRepository repository;
+
+    
+}
