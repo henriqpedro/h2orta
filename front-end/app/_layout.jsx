630a6f14016ce245e6c1f44be6254b2f055b0dfc
Pedro <114971311+henriqpedro@users.noreply.github.com>
2024-06-13 17:18:02 -0300

    Configurando projeto React

---
+++ front-end/app/_layout.jsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+const RootLayout = () => {
+
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+    );
+}
+
+export default RootLayout;
---
+++ front-end/app/index.jsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">H2Orta!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
