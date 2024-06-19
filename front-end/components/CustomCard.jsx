849e4715a46d732cb57672752550cbf0fd84c52f
Pedro <114971311+henriqpedro@users.noreply.github.com>
2024-06-19 09:22:19 -0300

    Implementando tela de planta

--- front-end/components/CustomCard.jsx
+++ front-end/components/CustomCard.jsx
@@ -1,6 +1,7 @@
 import { Entypo } from '@expo/vector-icons';
-import React, { useState } from 'react';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
+import * as Paho from 'paho-mqtt';
+import React, { useEffect, useState } from 'react';
+import { Image, Text, ToastAndroid, TouchableOpacity, View } from 'react-native';
 import CustomButton from './CustomButton';
 import CustomTextIcon from './CustomTextIcon';
 
@@ -20,40 +21,104 @@ const CustomCardField = ({ iconSource, title, value, containerStyles }) => {
     )
 }
 
-const CustomCard = ({ imageSource, name, description, humidity, tank, index }) => {
-
+const CustomCard = ({ item, index }) => {
+    const [watering, setWatering] = useState(false)
     const [showMenu, setShowMenu] = useState(false)
+    const [plantData, setPlantData] = useState({ humidity: 0, tank: 0 })
+
+    const client = new Paho.Client('h2orta.zapto.org', 8083, `client-${Math.random() * 1000}`)
+
+    const attemptConnection = (mensagem) => {
+        try {
+            client.connect({
+                keepAliveInterval: 20,
+                onSuccess() {
+                    client.subscribe('h2orta/planta')
+                    if (mensagem) {
+                        client.send(mensagem)
+                        ToastAndroid.show("Regando planta", ToastAndroid.SHORT)
+                    }
+                },
+                onFailure(error) {
+                    errorConnecting(error)
+                },
+            })
+        } catch (error) {
+            ToastAndroid.show("Erro ao comunicar com broker", ToastAndroid.SHORT)
+        }
+    }
+
+    const errorConnecting = (error) => {
+        ToastAndroid.show("Erro ao conectar MQTT: " + error.errorMessage, ToastAndroid.SHORT)
+        setTimeout(attemptConnection, 1000)
+    }
+
+    useEffect(() => {
+        attemptConnection()
+        client.onConnectionLost = errorConnecting
+        client.onMessageArrived = (message) => {
+            let data = message.payloadString.split(' ')
+            setPlantData({
+                humidity: data[0],
+                tank: data[1]
+            })
+        }
+        return () => client.disconnect()
+    }, [])
+
+    sendMessage = (message) => {
+        let mensagem = new Paho.Message(message)
+        mensagem.qos = 2
+        mensagem.destinationName = 'h2orta/irrigar'
+        attemptConnection(mensagem)
+    }
+
+    useEffect(() => {
+        if (watering) {
+            setShowMenu(false)
+            setTimeout(() => setWatering(false), 100)
+        }
+    }, [watering])
 
     return (
-        <View className="bg-secondary min-h-[50vh] min-w-[80vw] rounded-xl px-5 pt-8 pb-16 justify-center items-center" key={index}>
+        <View className="bg-secondary min-h-[50vh] min-w-[80vw] rounded-xl px-5 pt-2 pb-12 justify-center items-center" key={index}>
             <View className="justify-center items-center">
-                <TouchableOpacity activeOpacity={0.7}
+                <TouchableOpacity
+                    activeOpacity={0.7}
                     onPress={() => setShowMenu(!showMenu)}
-                    className='self-end min-h-[48px] px-5 justify-center items-end'>
+                    className='self-end min-h-[48px] px-5 top-2 justify-center items-end'>
                     <Entypo name="dots-three-horizontal" size={24} color="black" />
                     {
                         showMenu ?
                             <View className="absolute top-10 z-2">
-                                <CustomButton constainerStyles='bg-primary rounded-none' textStyles='text-black text-sm' title='Irrigar planta' />
+                                <CustomButton
+                                    isLoading={watering}
+                                    handlePress={() => {
+                                        sendMessage('1')
+                                        setWatering(true)
+                                    }}
+                                    constainerStyles='bg-primary rounded-none'
+                                    textStyles='text-black text-sm'
+                                    title='Irrigar planta' />
                             </View>
                             : <></>
                     }
                 </TouchableOpacity>
-                <Image className="w-[150px] h-[150px]" source={imageSource} resizeMode='contain' />
-                <Text className="text-dark text-xl">{name}</Text>
-                <Text className="text-center px-10 mt-2">{description}</Text>
+                <Image className="w-[150px] h-[150px]" source={item.imageSource} resizeMode='contain' />
+                <Text className="text-dark text-xl">{item.name}</Text>
+                <Text className="text-center px-10 mt-2">{item.description}</Text>
             </View>
             <View>
                 <CustomCardField
                     containerStyles="mt-5"
                     iconSource={require('../assets/icons/tank.png')}
                     title="Nível do reservatório"
-                    value={tank} />
+                    value={plantData.tank} />
                 <CustomCardField
                     containerStyles="mt-2"
                     iconSource={require('../assets/icons/humidity.png')}
                     title="Umidade"
-                    value={humidity} />
+                    value={plantData.humidity} />
             </View>
         </View>
     )
