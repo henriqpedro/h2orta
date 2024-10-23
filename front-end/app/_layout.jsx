8dcabbf13c31d9127add20a9c5a39f577a7320cc
JoaoOliveiraZz <joaolima.gxp@gmail.com>
2024-10-23 16:07:34 -0300

    Tela de cadastro de plantas e integração de novos sensores
    Novas Funcionalidades:
    - Rota de Cadastro de Plantas: Adicionada nova rota na stack para o registro de plantas.
    - Campo de Imagem Dinâmico: A imagem no campo muda automaticamente com base na planta selecionada.
    - Botões de Conectividade: Botões de Bluetooth e Wi-Fi são exibidos de forma dinâmica de acordo com a disponibilidade de sensores.
    - Modal de Seleção de Plantas: Ao clicar no campo "Planta", abre-se um modal que permite selecionar a planta desejada.
    
    Novos Componentes:
    - Lista Personalizável: Implementado componente de lista com ícone e subtítulo, reutilizável em diferentes partes do aplicativo.
    
    Integração de Novos Sensores:
    - Sensores de Temperatura e Umidade: Agora o app captura e exibe dados dos novos sensores de temperatura e umidade do ar.
    - Exibição no Card de Planta: Os dados dos sensores são exibidos diretamente no card de cada planta na tela inicial, facilitando o monitoramento.

--- front-end/app/_layout.jsx
+++ front-end/app/_layout.jsx
@@ -3,9 +3,18 @@ import { AuthProvider } from '../context/AuthContext';
 
 export const RootStack = () => {
     return (
-        <Stack>
+        <Stack
+            screenOptions={
+                {
+                    headerStyle: {
+                        backgroundColor: "#F9F9F9",
+                    }
+                }
+            }
+        >
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="home" options={{ headerShown: false }} />
+            <Stack.Screen name="plants/newPlant" options={{ title: "Nova planta", headerTintColor:"#76974B"}} />
             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
     );
---
+++ front-end/utils/plants.js
@@ -0,0 +1,38 @@
+export const plantsData = [
+    {
+        id: 1,
+        name: 'Pimenta',
+        description: 'As pimentas do gênero Capsicum (em grego, Kapso significa picar ou arder) são procedentes do continente americano.',
+        imageSource: require("../assets/images/pimenta.png")
+    },
+    {
+        id: 1,
+        name: 'Violeta',
+        description: 'Herbácea perene de até 20 cm de altura. Existem diversos híbridos com flores, de cores, tamanhos e formas variadas que despontam quase o ano inteiro.',
+        imageSource: require("../assets/images/violet.png")
+    },
+    {
+        id: 1,
+        name: 'Samambaia',
+        description: ' As samambaias são em geral plantas herbáceas, rizomatosas com folhas longas, subdivididas em folíolos que podem ser lisos ou rendados.',
+        imageSource: require("../assets/images/samambaia.png")
+    },
+    {
+        id: 1,
+        name: 'Lírio',
+        description: 'Os lírios são flores antigas com diversas variações para surpreender todos os gostos. São conhecidos pelo forte perfume, por isso onde essas flores estão não há quem não note.',
+        imageSource: require("../assets/images/lirio.png")
+    },
+    {
+        id: 1,
+        name: 'Antúrio',
+        description: 'Chamada também de “planta coração”, o antúrio tem a coloração vermelha ou alaranjada e um formato que se assemelha a um coração, com uma haste amarela no centro.',
+        imageSource: require("../assets/images/anturio.png")
+    },
+    {
+        id: 1,
+        name: 'Espada de são jorge',
+        description: 'A espada-de-são-jorge é uma planta herbácea, cespitosa, acaule e rizomatosa, que pode atingir até 90 cm de altura.',
+        imageSource: require("../assets/images/espada-de-sao-jorge.png")
+    }
+]
