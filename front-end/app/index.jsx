a44826db685593ccd7ec4e0b5c943b5a01081c60
Pedro <114971311+henriqpedro@users.noreply.github.com>
2025-06-10 16:55:12 -0300

    Fix integration

--- front-end/app/index.jsx
+++ front-end/app/index.jsx
@@ -1,16 +1,13 @@
 import { useAuthContext } from '../context/AuthContext';
-import Home from './home';
 import SignIn from './(auth)/sign-in';
-import { PaperProvider } from 'react-native-paper';
+import Home from './home';
 
 export default function App() {
   const { authState } = useAuthContext();
 
   return (
-    <PaperProvider>
-      {authState.autenticated ?
-        <Home></Home> :
-        <SignIn></SignIn>}
-    </PaperProvider>
+    authState.autenticated ?
+      <Home></Home> :
+      <SignIn></SignIn>
   );
 }
