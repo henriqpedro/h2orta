52b86bb0e0657a67f5abbaf74708bde9ee47b063
Pedro Henrique <pedropereira712@gmail.com>
2025-11-10 12:58:54 -0300

    finishing config

--- back-end/src/main/java/com/example/h2orta/config/WebConfig.java
+++ back-end/src/main/java/com/example/h2orta/config/WebConfig.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCoresMapping(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
